d0a546d1e455cd97525b3d91d6c4b9be1aa315cc
Lena <lenazozula05@gmail.com>
2023-09-05 22:28:36 +0300

    Добавьте файлы проекта.

---
+++ CopyFilesWPF/Model/FilePath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFilesWPF.Model
+{
+    public sealed record FilePath
+    {
+        private string pathTo { get; set; } = string.Empty;
+
+        public string pathFrom { get; set; } = string.Empty;
+
+        public string PathTo
+        {
+            get => pathTo + "\\" + Path.GetFileName(pathFrom);
+            set => pathTo = value;
+        }
+    }
+}
---
+++ CopyFilesWPF/Presenter/IMainWindowPresenter.cs
@@ -0,0 +1,11 @@
+﻿namespace CopyFilesWPF.Presenter
+{
+    public interface IMainWindowPresenter
+    {
+        void CopyButtonClick();
+
+        void ChooseFileToButtonClick(string path);
+
+        void ChooseFileFromButtonClick(string path);
+    }
+}
